6a7f170c22b2b5b0c57af9902c43601afbe701f6
julialopwz <julialopes@usp.br>
2021-02-09 20:15:55 +0000

    slick

---
+++ public/JS/script.js
@@ -0,0 +1,10 @@
+$(document).on('ready', function() {
+
+    $('.multiple-items').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 7,
+        slidesToScroll: 7
+    });
+});
